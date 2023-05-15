3377c16b7761dd31c36ff966d93c27a1ead96248
Rikky Hermanto <rikky.hermanto@gmail.com>
2023-05-15 12:28:34 +0800

    Xunits for uservalidator and CreditLimitValidator added.

---
+++ LegacyApp.Tests/CreditLimitValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests
+{
+    public class CreditLimitValidatorTests
+    {
+        [Fact]
+        public void Validate_ReturnsTrue_WhenUserHasNoCreditLimit()
+        {
+            // Arrange
+            var user = new User { Firstname = "John", Surname = "Doe", EmailAddress = "john.doe@example.com", DateOfBirth = new DateTime(1990, 1, 1), HasCreditLimit = false };
+
+            var validator = new CreditLimitValidator();
+
+            // Act
+            var result = validator.Validate(user);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Validate_ReturnsTrue_WhenUserHasCreditLimitGreaterThan500()
+        {
+            // Arrange
+            var user = new User { Firstname = "John", Surname = "Doe", EmailAddress = "john.doe@example.com", DateOfBirth = new DateTime(1990, 1, 1), HasCreditLimit = true, CreditLimit = 1000 };
+
+            var validator = new CreditLimitValidator();
+
+            // Act
+            var result = validator.Validate(user);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Validate_ReturnsFalse_WhenUserHasCreditLimitLessThan500()
+        {
+            // Arrange
+            var user = new User { Firstname = "John", Surname = "Doe", EmailAddress = "john.doe@example.com", DateOfBirth = new DateTime(1990, 1, 1), HasCreditLimit = true, CreditLimit = 200 };
+
+            var validator = new CreditLimitValidator();
+
+            // Act
+            var result = validator.Validate(user);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ LegacyApp.Tests/UserValidatorTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Xunit;
+
+namespace LegacyApp.Tests
+{
+    public class UserValidatorTests
+    {
+        [Fact]
+        public void Validate_ValidUser_ReturnsTrue()
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new User
+            {
+                Firstname = "John",
+                Surname = "Doe",
+                EmailAddress = "john.doe@example.com",
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };
+
+            // Act
+            bool result = validator.Validate(user);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Validate_InvalidName_ReturnsFalse(string name)
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new User
+            {
+                Firstname = name,
+                Surname = "Doe",
+                EmailAddress = "john.doe@example.com",
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };
+
+            // Act
+            bool result = validator.Validate(user);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("john.doe")]
+        [InlineData("johndoe@")]
+        [InlineData("johndoe@examplecom")]
+        public void Validate_InvalidEmail_ReturnsFalse(string email)
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new User
+            {
+                Firstname = "John",
+                Surname = "Doe",
+                EmailAddress = email,
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };
+
+            // Act
+            bool result = validator.Validate(user);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("2010-01-01", false)]
+        [InlineData("2000-01-01", true)]
+        public void Validate_InvalidAge_ReturnsFalse(string dob, bool expected)
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new User
+            {
+                Firstname = "John",
+                Surname = "Doe",
+                EmailAddress = "john.doe@example.com",
+                DateOfBirth = DateTime.Parse(dob)
+            };
+
+            // Act
+            bool result = validator.Validate(user);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
